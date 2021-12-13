ad67090ca1520a768b129ccf8be37b618d436a2a
Md-Sarfaraz <sarfarazraj217@gmail.com>
2021-12-14 01:14:52 +0530

    commit

--- src/fragments/NotePad.jsx
+++ src/fragments/NotePad.jsx
@@ -1,51 +1,95 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import remarkGfm from 'remark-gfm'
 import style from 'react-syntax-highlighter/dist/esm/styles/prism/coldark-cold';
 import useLocalStorage from '../services/StorageHook';
-
+import { downloadMarkdownAsFile } from '../services/Util';
+import './notepad.css'
+import axios, { post } from 'axios';
 
 const NotePad = () => {
 
-
-    const [markdown, setMarkdown] = useLocalStorage('mdkey',"Welcome to Markdwon Editor")
+    const inputRef = useRef("")
+    const fullView = useRef()
+    const editorRef = useRef("")
+    const [markdown, setMarkdown] = useLocalStorage('mdkey', "Welcome to Markdwon Editor")
     useEffect(() => {
+        console.log(inputRef.current.scrollHeight)
+        // inputRef.current.setAttribute('style', 'height:' + (inputRef.current.scrollHeight) + 'px');
+        inputRef.current.style.height = 'auto';
+        inputRef.current.style.height = (inputRef.current.scrollHeight) + 'px';
 
-    }, [])
+        const editor = editorRef.current
+        if (editor) {
+            //editor.scrollTop = editor.scrollHeight
+            //editor.scrollIntoView()
+        }
+    }, [markdown])
+
+    function downloadFile(e) {
+        const contentType = { type: "text/plain;charset=utf-8" }
+        downloadMarkdownAsFile(markdown, contentType, "newMarkdown.md", e.target)
+    }
+    function uploadFile(e) {
+        let files = e.target.files
+        let reader = new FileReader()
+        reader.readAsText(files[0])
+
+        reader.onload = (e) => {
+            setMarkdown(e.target.result)
+
+        }
+
+    }
 
     function preventTabExit(e) {
         if (e.key === 'Tab' && !e.shiftKey) {
             document.execCommand('insertText', false, "\t");
+
             e.preventDefault();
             return false;
         }
     }
+
+    function toogleFullWidth(e) {
+        fullView.current.classList.toggle('container')
+    }
     return (
+
         <div className="content-wrapper">
             <section className="content-header">
-            <div className="container">
-                            <div className="row mb-2">
-                                <div className="col-sm-6">
-                                    <h1 className="m-0 text-dark"> Raspberry Pie <small>System Monitor</small></h1>
-                                </div>
-                                <div className="col-sm-6 d-flex flex-row justify-content-end">
-                                    <button className="btn btn-info" >Connect</button>
-                                </div>
-
-                            </div>
+                <div className="container">
+                    <div className="row mb-2">
+                        <div className="col-sm-6">
+                            <h1 className="m-0 text-dark"><strong>Notepad</strong> <small className='ml-3'>(Markdown Editor)</small></h1>
                         </div>
+                        <div className="col-sm-6 d-flex justify-content-end">
+                            <input type="button" onClick={toogleFullWidth} className='btn btn-success' value="Full Width" />
+                        </div>
+                    </div>
+                </div>
             </section>
             <div className="content">
-                <div className=''>
-                    <div className="card my-3" style={{height:'100%'}}>
-                        <div className="card-header">
-                            <h4>MarkDown Editor</h4>
+                <div className='container-fluid' ref={fullView}>
+                    <div className="card my-3" style={{ height: '100%' }}>
+                        <div className="card-header bg-info">
+                            <h4 className='card-title'>MarkDown Editor</h4>
+                            <div class="card-tools">
+
+                                <label for="mdfile" className='btn btn-info mb-0'>
+                                    <i class="fas fa-file-upload" style={{ fontSize: "1.2em" }}> Upload</i>
+                                </label>
+                                <input type="file" name="mdfile" id='mdfile' className="upload-hidden btn btn-warning mr-4" onChange={uploadFile} />
+                                <a onClick={downloadFile} className=" btn btn-info" >
+                                    <i class="fas fa-file-download" style={{ fontSize: "1.2em" }}> Download</i></a>
+                            </div>
                         </div>
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-md-6  p-2"   >
-                                    <textarea className="form-group" style={{height:'90%',maxHeight:'100%',minHeight:'100%', width: '100%'}}value={markdown} onKeyDown={preventTabExit} onChange={(e) => { setMarkdown(e.target.value) }} ></textarea>
+                        <div className="card-body h-100">
+                            <div ref={editorRef} className="row">
+                                <div className="col-md-6 md-preview p-2"   >
+                                    <textarea ref={inputRef} className="form-group " style={{ height: '100%', minHeight: '100%', width: '100%' }}
+                                        value={markdown} onKeyDown={preventTabExit} onChange={(e) => { setMarkdown(e.target.value) }} ></textarea>
                                 </div>
                                 <div className="col-md-6">
                                     <ReactMarkdown
--- src/services/Util.jsx
+++ src/services/Util.jsx
@@ -12,3 +12,13 @@ export function ValidatePort(num) {
     const regexExp = /^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$/gi;
     return regexExp.test(num);
 }
+
+export function downloadMarkdownAsFile(content, contentType, filename, anchorRef) {
+    const file = new Blob([content], { type: contentType });
+    anchorRef.href =  URL.createObjectURL(file)
+    anchorRef.download = filename
+}
+
+export function uploadMarkdownAsFile(){
+
+}
